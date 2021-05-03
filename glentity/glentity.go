2927e2b3992c175b5db958747e56105860818e87
Frediansah <frediansah>
2021-05-03 08:46:19 +0700

    add base entity migrate version

--- glentity/glentity.go
+++ glentity/glentity.go
@@ -1,6 +1,9 @@
 package glentity
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 type BaseEntity struct {
 	CreateDatetime string `json:"createDatetime"`
@@ -29,10 +32,37 @@ type BaseEntityTs struct {
 	Version         int64     `json:"version"`
 }
 
+type BaseEntityMigrateTs struct {
+	CreateDatetime  string    `json:"createDatetime"`
+	UpdateDatetime  string    `json:"updateDatetime"`
+	CreateTimestamp time.Time `json:"createTimestamp"`
+	UpdateTimestamp time.Time `json:"updateTimestamp"`
+	CreateUserId    int64     `json:"createUserId"`
+	UpdateUserId    int64     `json:"updateUserId"`
+	Version         int64     `json:"version"`
+}
+
 type MasterEntityTs struct {
-	Active             string `json:"active"`
-	ActiveTimestamp    string `json:"activeTimestamp"`
-	NonActiveTimestamp string `json:"nonActiveTimestamp"`
+	Active             string       `json:"active"`
+	ActiveTimestamp    sql.NullTime `json:"activeTimestamp"`
+	NonActiveTimestamp sql.NullTime `json:"nonActiveTimestamp"`
+
+	CreateTimestamp time.Time `json:"createTimestamp"`
+	UpdateTimestamp time.Time `json:"updateTimestamp"`
+	CreateUserId    int64     `json:"createUserId"`
+	UpdateUserId    int64     `json:"updateUserId"`
+	Version         int64     `json:"version"`
+}
+
+type MasterEntityMigrateTs struct {
+	ActiveDatetime    string `json:"activeDatetime"`
+	NonActiveDatetime string `json:"nonActiveDatetime"`
+	CreateDatetime    string `json:"createDatetime"`
+	UpdateDatetime    string `json:"updateDatetime"`
+
+	Active             string       `json:"active"`
+	ActiveTimestamp    sql.NullTime `json:"activeTimestamp"`
+	NonActiveTimestamp sql.NullTime `json:"nonActiveTimestamp"`
 
 	CreateTimestamp time.Time `json:"createTimestamp"`
 	UpdateTimestamp time.Time `json:"updateTimestamp"`
